1876ad4efa1a83b134ad05512d7811d25ad25882
LuigiVanin <luisfvaninmartins@gmail.com>
2022-05-12 17:57:05 -0300

    feat: auth context com componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Reset from "./styles/reset.style";
 import GlobalStyles from "./styles/global.style";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
 import SignUp from "./components/SignUp.jsx";
 
 const App = () => {
@@ -9,14 +10,16 @@ const App = () => {
             <BrowserRouter>
                 <Reset />
                 <GlobalStyles />
-                <Routes>
-                    <Route path="/sign-up" element={<SignUp />} />
-                    {/* <Route path="/" element={<SignIn />} />
+                <AuthProvider>
+                    <Routes>
+                        <Route path="/sign-up" element={<SignUp />} />
+                        {/* <Route path="/" element={<SignIn />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/categories" element={<Categories />} />
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/success" element={<Success />} /> */}
-                </Routes>
+                    </Routes>
+                </AuthProvider>
             </BrowserRouter>
         </>
     );
